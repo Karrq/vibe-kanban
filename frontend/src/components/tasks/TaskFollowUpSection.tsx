d423405d9527c2945ad6a30e5a47affdb41d7078
Francesco Dainese <franci.dainese@gmail.com>
2025-08-18 14:53:16 +0200

    fix: allow drafting followups

--- frontend/src/components/tasks/TaskFollowUpSection.tsx
+++ frontend/src/components/tasks/TaskFollowUpSection.tsx
@@ -140,7 +140,7 @@ export function TaskFollowUpSection() {
                 }
               }}
               className="flex-1 min-h-[40px] resize-none"
-              disabled={!canSendFollowUp}
+              disabled={false}
               projectId={projectId}
               rows={1}
               maxRows={6}
